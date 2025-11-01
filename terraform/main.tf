9c7c96fbef5881c90749e6f85f23feeec41c6d67
acckiymops <meleshkin87@gmail.com>
2025-11-01 22:36:24 +0300

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.51.0"
+    }
+  }
+  required_version = "~>1.12.0"
+}
+provider "docker" {
+  host = "ssh://mvmeles1@158.160.203.216"
+}
+
+resource "random_password" "mysql_root_password" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+ 
+resource "random_password" "mysql_password" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+resource "docker_image" "mysql" {
+  name         = "mysql:8"
+  keep_locally = false
+}
+
+resource "docker_container" "db" {
+  image = docker_image.mysql.image_id
+  name  = "wordpress"
+
+  env = [
+    "MYSQL_ROOT_PASSWORD=${random_password.mysql_root_password.result}",
+    "MYSQL_DATABASE=wordpress",
+    "MYSQL_USER=wordpress",
+    "MYSQL_PASSWORD=${random_password.mysql_password.result}",
+    "MYSQL_ROOT_HOST=%"
+  ]
+
+  ports {
+    internal = 3306
+    external = 3306
+    ip       = "127.0.0.1"
+  }
+
+  restart = "always"
+}
